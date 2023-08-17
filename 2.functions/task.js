a654152bc6f769d6acc4f12f72d87ebb407575a0
lexxx3105 <129284703+lexxx3105@users.noreply.github.com>
2023-08-17 11:22:04 +0300

    первое задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,17 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+
+    sum += arr[i]
+  }
+
+  const meaningAvg = sum / arr.length;
+  const avg = Number(meaningAvg.toFixed(2))
 
   return { min: min, max: max, avg: avg };
 }
